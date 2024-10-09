61aba470e4f9ab4d8ee4a2d10f2ae4313b2022da
tmarcorf <tmarco783@gmail.com>
2024-10-09 18:14:59 -0300

    feat: adicionando pacote class library ao Nuget

---
+++ Marco.HATEOAS/Marco.HATEOAS/Hypermedia/Abstract/IResponseEnricher.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Marco.HATEOAS.Hypermedia.Abstract
+{
+    public interface IResponseEnricher
+    {
+        bool CanEnrich(ResultExecutingContext context);
+        Task Enrich(ResultExecutingContext context);
+    }
+}
---
+++ Marco.HATEOAS/Marco.HATEOAS/Hypermedia/Filters/HyperMediaFilterOptions.cs
@@ -0,0 +1,10 @@
+﻿using Marco.HATEOAS.Hypermedia.Abstract;
+using System.Collections.Generic;
+
+namespace Marco.HATEOAS.Hypermedia.Filters
+{
+    public class HyperMediaFilterOptions
+    {
+        public List<IResponseEnricher> ContentResponseEnricherList { get; set; } = new List<IResponseEnricher>();
+    }
+}
