4e759cadd17626c95782d53f47130e1d88fb7810
tmarcorf <tmarco783@gmail.com>
2024-10-09 18:25:47 -0300

    refactor: movendo projeto Marco.HATEOAS para um novo repositório

--- Marco.HATEOAS/RestWithASPNETErudio/Data/Converter/Contract/IParser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RestWithASPNETErudio.Data.Converter.Contract
-{
-    public interface IParser<O, D>
-    {
-        D Parse(O origin);
-        List<D> Parse(List<O> origin);
-    }
-}
--- Marco.HATEOAS/RestWithASPNETErudio/Data/VO/UserVO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RestWithASPNETErudio.Data.VO
-{
-    public class UserVO
-    {
-        public string Password { get; set; }
-        public string UserName { get; set; }
-    }
-}
